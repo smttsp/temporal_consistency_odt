62a09f9a6c16bdefd127f4c2d2defa9acec3b812
Samet <samettaspinarx@gmail.com>
2023-09-17 13:00:11 -0400

    [UPD]`check_if_missing_or_extra_object` func added

--- temporal_consistency/frame_anomaly_detection.py
+++ temporal_consistency/frame_anomaly_detection.py
@@ -42,3 +42,21 @@ class FrameInfoList:
             or self.check_incorrect_object_classification()
         )
 
+    def check_if_missing_or_extra_object(self):
+        """This checks if there is any object that is missing in the middle
+        but exists in the other two. Or middle one contains on object
+        that is not in the other ones.
+        """
+        pass
+        prev_ids, mid_ids, next_ids = [
+            lst.object_ids for lst in self.frame_info_list
+        ]
+
+        # Find elements in mid_ids that aren't in prev_ids or next_ids
+        extra_elements_in_mid = mid_ids - (prev_ids.union(next_ids))
+
+        # Find elements that are in both prev_ids and next_ids but not in mid_ids
+        missing_elements_in_mid = (prev_ids.intersection(next_ids)) - mid_ids
+
+        return extra_elements_in_mid or missing_elements_in_mid
+
