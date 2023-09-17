a7f72802838c2ed031f0a2f26020588c633f057d
Samet <samettaspinarx@gmail.com>
2023-09-16 20:26:00 -0400

    [UPD] made the code more functional

--- temporal_consistency/object_detection_tracking.py
+++ temporal_consistency/object_detection_tracking.py
@@ -9,24 +9,10 @@ CONFIDENCE_THRESHOLD = 0.8
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 
-# initialize the video capture object
-video_cap = cv2.VideoCapture("video2.mp4")
-# initialize the video writer object
-writer = create_video_writer(video_cap, "output.mp4")
-
-# load the pre-trained YOLOv8n model
 model = YOLO("yolov8n.pt")
-tracker = DeepSort(max_age=50)
 
 
-while True:
-    start = datetime.datetime.now()
-
-    ret, frame = video_cap.read()
-
-    if not ret:
-        break
-
+def object_detection(model, frame):
     # run the YOLO model on the frame
     detections = model(frame)[0]
 
@@ -60,7 +46,10 @@ while True:
         results.append(
             [[xmin, ymin, xmax - xmin, ymax - ymin], confidence, class_id]
         )
+    return results
 
+
+def object_tracking(frame, results, tracker):
     ######################################
     # TRACKING
     ######################################
@@ -95,25 +84,51 @@ while True:
             WHITE,
             2,
         )
+    return frame
 
-    # end time to compute the fps
-    end = datetime.datetime.now()
-    # show the time it took to process 1 frame
-    print(
-        f"Time to process 1 frame: {(end - start).total_seconds() * 1000:.0f} milliseconds"
-    )
-    # calculate the frame per second and draw it on the frame
-    fps = f"FPS: {1 / (end - start).total_seconds():.2f}"
-    cv2.putText(
-        frame, fps, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 8
-    )
 
-    # show the frame to our screen
-    cv2.imshow("Frame", frame)
-    writer.write(frame)
-    if cv2.waitKey(1) == ord("q"):
-        break
+def object_detection_and_tracking(
+    model,
+    video_filepath="video2.mp4",
+):
+    # initialize the video capture object
+    video_cap = cv2.VideoCapture(video_filepath)
+    output_filepath = video_filepath.replace(".mp4", "_output.mp4")
 
-video_cap.release()
-writer.release()
-cv2.destroyAllWindows()
+    # initialize the video writer object
+    writer = create_video_writer(video_cap, output_filepath)
+
+    # load the pre-trained YOLOv8n model
+    tracker = DeepSort(max_age=50)
+
+    while True:
+        start = datetime.datetime.now()
+
+        ret, frame = video_cap.read()
+
+        if not ret:
+            break
+
+        results = object_detection(model, frame)
+        frame = object_tracking(frame, results, tracker)
+
+        end = datetime.datetime.now()
+        # show the time it took to process 1 frame
+        total = (end - start).total_seconds() * 1000
+        print(f"Time to process 1 frame: {total:.0f} milliseconds")
+
+        # calculate the frame per second and draw it on the frame
+        fps = f"FPS: {1 / (end - start).total_seconds():.2f}"
+        cv2.putText(
+            frame, fps, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 8
+        )
+
+        # show the frame to our screen
+        cv2.imshow("Frame", frame)
+        writer.write(frame)
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    video_cap.release()
+    writer.release()
+    cv2.destroyAllWindows()
