f9859a6859b38188571a95dfe52822c9f160462e
Samet <samettaspinarx@gmail.com>
2023-09-16 20:25:42 -0400

    [CLN] removed unnecessary codes

--- tests_scripting/reinstall_package.sh
+++
@@ -1,5 +0,0 @@
-# Convenience Script to use after `install_deps.sh` has run at least once
-# If you delete or modify the Python virtualenv, you'll need to re-run
-#   the `install_deps.sh` script, instead of just using this one.
-
-pip install ..
