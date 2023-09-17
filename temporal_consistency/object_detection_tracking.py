cc00192552e27c3aabcbfe96a521c565564a678c
Samet <samettaspinarx@gmail.com>
2023-09-16 20:51:47 -0400

    [UPD] `draw_bbox_around_object` function extracted

--- temporal_consistency/object_detection_tracking.py
+++ temporal_consistency/object_detection_tracking.py
@@ -1,9 +1,10 @@
+import copy
 import datetime
+
 from ultralytics import YOLO
 import cv2
 from helper import create_video_writer
 from deep_sort_realtime.deepsort_tracker import DeepSort
-
 
 CONFIDENCE_THRESHOLD = 0.7
 GREEN = (0, 255, 0)
@@ -29,7 +30,7 @@ def object_detection(model, frame):
 
         # if the confidence is greater than the minimum confidence,
         # get the bounding box and the class id
-        xmin, ymin, xmax, ymax = (
+        x_min, y_min, x_max, y_max = (
             int(data[0]),
             int(data[1]),
             int(data[2]),
@@ -38,9 +39,33 @@ def object_detection(model, frame):
         class_id = int(data[5])
         # add the bounding box (x, y, w, h), confidence and class id to the results list
         results.append(
-            [[xmin, ymin, xmax - xmin, ymax - ymin], confidence, class_id]
+            [[x_min, y_min, x_max - x_min, y_max - y_min], confidence, class_id]
         )
     return results
+
+
+def draw_bbox_around_object(frame, track, voc_bbox):
+    track_id = track.track_id
+
+    x_min, y_min, x_max, y_max = (
+        int(voc_bbox[0]),
+        int(voc_bbox[1]),
+        int(voc_bbox[2]),
+        int(voc_bbox[3]),
+    )
+    # draw the bounding box and the track id
+    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), GREEN, 2)
+    cv2.rectangle(frame, (x_min, y_min - 20), (x_min + 20, y_min), GREEN, -1)
+    cv2.putText(
+        frame,
+        str(track_id),
+        (x_min + 5, y_min - 8),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.5,
+        WHITE,
+        2,
+    )
+    return None
 
 
 def object_tracking(frame, results, tracker):
@@ -54,27 +79,8 @@ def object_tracking(frame, results, tracker):
             continue
 
         # get the track id and the bounding box
-        track_id = track.track_id
-        ltrb = track.to_ltrb()
-
-        xmin, ymin, xmax, ymax = (
-            int(ltrb[0]),
-            int(ltrb[1]),
-            int(ltrb[2]),
-            int(ltrb[3]),
-        )
-        # draw the bounding box and the track id
-        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), GREEN, 2)
-        cv2.rectangle(frame, (xmin, ymin - 20), (xmin + 20, ymin), GREEN, -1)
-        cv2.putText(
-            frame,
-            str(track_id),
-            (xmin + 5, ymin - 8),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            WHITE,
-            2,
-        )
+        voc_bbox = track.to_ltrb()
+        draw_bbox_around_object(frame, track, voc_bbox)
     return frame
 
 
@@ -88,7 +94,7 @@ def object_detection_and_tracking(model, video_filepath):
 
     # load the pre-trained YOLOv8n model
     tracker = DeepSort(max_age=50)
-
+    tracker_list: list[DeepSort] = []
     while True:
         start = datetime.datetime.now()
 
@@ -99,7 +105,7 @@ def object_detection_and_tracking(model, video_filepath):
 
         results = object_detection(model, frame)
         frame = object_tracking(frame, results, tracker)
-
+        tracker_list.append(copy.deepcopy(tracker))
         end = datetime.datetime.now()
         # show the time it took to process 1 frame
         total = (end - start).total_seconds() * 1000
@@ -120,8 +126,12 @@ def object_detection_and_tracking(model, video_filepath):
     video_cap.release()
     writer.release()
     cv2.destroyAllWindows()
+    return tracker_list
 
 
 if __name__ == "__main__":
     model = YOLO("yolov8n.pt")
-    object_detection_and_tracking(model, video_filepath="video2.mp4")
+    tracker_list = object_detection_and_tracking(
+        model, video_filepath="video2.mp4"
+    )
+    print()
