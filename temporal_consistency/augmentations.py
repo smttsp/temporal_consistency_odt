05190ad6c9a290050d884029a711c4f1b99e0176
Samet <samettaspinarx@gmail.com>
2023-09-17 20:31:51 -0400

    [UPD] added augmentations.py, doing rand augmentations

---
+++ temporal_consistency/augmentations.py
@@ -0,0 +1,62 @@
+import random
+from albumentations import (
+    Compose,
+    RandomBrightnessContrast,
+    RandomGamma,
+    ColorJitter,
+    ChannelShuffle,
+    RGBShift,
+    Blur,
+    GaussNoise,
+    RandomRain,
+    RandomSnow,
+    RandomSunFlare,
+    RandomFog,
+    RandomShadow,
+    InvertImg,
+    RandomToneCurve,
+    Solarize,
+    Equalize,
+    Posterize,
+)
+import matplotlib.pyplot as plt
+
+
+def get_aug_list():
+    return [
+        RandomBrightnessContrast(p=random.uniform(0.5, 1.0)),
+        RandomGamma(p=random.uniform(0.5, 1.0)),
+        ColorJitter(
+            brightness=random.uniform(0.1, 0.4),
+            contrast=random.uniform(0.1, 0.5),
+            saturation=random.uniform(0.1, 0.5),
+            hue=random.uniform(0.1, 0.5),
+            p=random.uniform(0.5, 1.0),
+        ),
+        ChannelShuffle(p=random.uniform(0.5, 1.0)),
+        RGBShift(p=random.uniform(0.5, 1.0)),
+        Blur(blur_limit=3, p=random.uniform(0.5, 1.0)),
+        GaussNoise(p=random.uniform(0.5, 1.0)),
+        RandomRain(p=random.uniform(0.5, 1.0)),
+        RandomSnow(p=random.uniform(0.5, 1.0)),
+        RandomSunFlare(p=random.uniform(0.5, 1.0), flare_roi=(0, 0, 1, 0.5)),
+        RandomFog(p=random.uniform(0.5, 1.0)),
+        RandomShadow(p=random.uniform(0.5, 1.0)),
+        InvertImg(p=random.uniform(0.5, 1.0)),
+        RandomToneCurve(p=random.uniform(0.5, 1.0)),
+        Solarize(p=random.uniform(0.5, 1.0)),
+        Equalize(p=random.uniform(0.5, 1.0)),
+        Posterize(p=random.uniform(0.5, 1.0)),
+    ]
+
+
+def get_random_augmentation(image, num_aug=1):
+    augmentation_pipeline = Compose(random.sample(get_aug_list(), k=num_aug))
+    image_aug = augmentation_pipeline(image=image)["image"]
+    # plt.subplot(1, 2, 1)
+    # plt.imshow(image)
+    # plt.subplot(1, 2, 2)
+    # plt.imshow(image_aug)
+    # plt.show()
+
+    return image_aug
