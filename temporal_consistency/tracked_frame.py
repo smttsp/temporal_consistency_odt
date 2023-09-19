66cc4c6a4faf3638e11fbffbf1535d351c1c59fa
Samet <samettaspinarx@gmail.com>
2023-09-19 13:24:29 -0400

    [UPD] `__get_class_name_from_track` func created

--- temporal_consistency/tracked_frame.py
+++ temporal_consistency/tracked_frame.py
@@ -3,6 +3,9 @@ import os
 from collections import defaultdict
 from utils import create_video_writer
 import numpy
+from vis_utils import draw_class_name
+
+OUT_FOLDER = "/users/samet/desktop/output/"
 
 
 class TrackedFrame:
@@ -18,14 +21,12 @@ class TrackedFrame:
 
 
 class TrackedFrameCollection:
-    def __init__(self, video_cap, output_folder="/users/samet/desktop/output/"):
-        self.out_folder = output_folder
-        os.makedirs(output_folder)
-
+    def __init__(self, video_cap, classes, out_folder=OUT_FOLDER):
+        self.video_cap = video_cap
+        self.out_folder = out_folder
+        self.classes = classes
         self.tracked_frames = []
         self.all_objects = defaultdict(dict)
-        self.video_cap_dict = {}  # object_id -> video_cap
-        self.video_cap = video_cap
 
     def add_tracked_frame(self, tracked_frame: TrackedFrame):
         self.tracked_frames.append(tracked_frame)
@@ -37,13 +38,15 @@ class TrackedFrameCollection:
             self.all_objects[track.track_id].update(cur_dict)
 
     def export_all_objects(self):
+        os.makedirs(self.out_folder, exist_ok=True)
+
         for object_id in self.all_objects:
             filename = os.path.join(self.out_folder, f"obj_{object_id}.mp4")
             writer = create_video_writer(self.video_cap, filename)
             self.export_object(writer, object_id)
 
     def export_object(self, writer, object_id):
-        a_dict = self.all_objects[str(object_id)]
+        a_dict = self.all_objects[object_id]
 
         start_idx, end_idx = min(a_dict.keys()), max(a_dict.keys())
 
@@ -57,6 +60,15 @@ class TrackedFrameCollection:
             black_frame[y1 : y2 + 1, x1 : x2 + 1] = frame[
                 y1 : y2 + 1, x1 : x2 + 1
             ]
+            class_name = self.__get_class_name_from_track(idx, object_id)
+            draw_class_name(black_frame, a_dict[idx], object_id, class_name)
             writer.write(black_frame)
 
         writer.release()
+
+    def __get_class_name_from_track(self, idx, object_id):
+        tracks = self.tracked_frames[idx].tracker.tracks
+        class_ids = [t.det_class for t in tracks if t.track_id == object_id]
+        class_id = class_ids[0]
+        class_name = self.classes[class_id]
+        return class_name
